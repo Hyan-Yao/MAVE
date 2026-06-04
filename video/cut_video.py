6e45ff25685c93f54e732baa6641c4143aefa299
Alyssa-Qingyue <alyssa@whu.edu.cn>
2026-06-04 11:23:10 +0800

    Update cut_video.py

--- video/cut_video.py
+++ video/cut_video.py
@@ -1,8 +1,8 @@
 import subprocess
 import os
 
-input_video = r"D:\Desktop\llm_as_a_judge\data\llm\Zoom Class Meeting Downing Soc 220 2⧸18⧸2021 [i-w2sxYTQbc]_001.mp4"
-output_dir = r"D:\Desktop\llm_as_a_judge\data\llm\cut_10s"
+input_video = r""
+output_dir = r""
 
 os.makedirs(output_dir, exist_ok=True)
 
@@ -13,7 +13,7 @@ for i in range(18):
     output = f"{output_dir}/segment_{start}s_{end}s.mp4"
 
     cmd = [
-        r"D:\Desktop\llm_as_a_judge\data\llm\ffmpeg-8.0.1-full_build\bin\ffmpeg.exe",
+        r"",
         "-y",
         "-ss", str(start),
         "-t", "10",
